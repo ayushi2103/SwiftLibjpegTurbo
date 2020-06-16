81007f153bebdb19bf79bc819da2628310eb0bd3
Ayushi Tiwari <ayushitwari2103@gmail.com>
2020-06-16 12:46:58 -0400

    header updated

---
+++ Sources/libjpegturbo/include/libjpegFunctions.h
@@ -0,0 +1,14 @@
+//
+//  libjpegFunctions.h
+//  
+//
+//  Created by Ayushi Tiwari on 2020-06-16.
+//
+
+#ifndef libjpegFunctions_h
+#define libjpegFunctions_h
+#include <stdio.h>
+
+#endif /* libjpegFunctions_h */
+
+DLLEXPORT unsigned char *tjJPEGLoadImage(const char *filename, int *width, int align, int *height, int *pixelFormat, int *inSubsamp, int flags);
--- Sources/libjpegturbo/libjpegFunctions.c
+++ Sources/libjpegturbo/libjpegFunctions.c
@@ -5,6 +5,7 @@
 //  Created by Ayushi Tiwari on 2020-06-16.
 //
 
+#include "libjpegFunctions.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
